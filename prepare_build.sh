54daab0a59d092f10c6c58a010cec4a831c123bf
tgdpesi2 <simon.peter@swisscom.com>
2018-11-15 16:07:58 +0100

    do not use bash

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 cp truststore_preparation.sh credhub/
 cp fetch-version.sh credhub/
-pushd credhub
+cd credhub
 ./truststore_preparation.sh
 ./gradlew --no-daemon assemble
-popd
